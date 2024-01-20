8fc0aeba469e2d89f4aa479bae8c202d2a181d52
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2024-01-20 21:22:11 +0200

    intial fetch

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,15 +2,16 @@
   <div id="app">
     <div ref="stage" id="container"></div>
     <ToolBar @addMachine="addMachine" @addQueue="addQueue" @addArrow="isAddingArrow = !isAddingArrow"
-      @addProduct="NumberofProducts = NumberofProducts + 1" @remProduct="NumberofProducts = NumberofProducts - 1" @play="play"
-      @replay="replay" :isAddingArrow="isAddingArrow" :NumberofProducts="NumberofProducts" />
+      @addProduct="NumberofProducts = NumberofProducts + 1" @remProduct="NumberofProducts = NumberofProducts - 1"
+      @play="play" @replay="replay" :isAddingArrow="isAddingArrow" :NumberofProducts="NumberofProducts" />
   </div>
 </template>
 
 <script>
 import ToolBar from './components/ToolBar.vue'
 import Konva from 'konva'
-
+import { Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 export default {
   name: 'App',
   components: {
@@ -18,11 +19,13 @@ export default {
   },
   data() {
     return {
+      stompClient: null,
       stage: null,
       layer: null,
       machines: [],
       queues: [],
       arrows: [],
+      arrowsGroup: [],
       nextMachineId: 1,
       nextQueueId: 0,
       isAddingArrow: false,
@@ -30,6 +33,18 @@ export default {
       selectedDestination: null,
       NumberofProducts: 0,
     }
+  },
+  created() {
+    const socket = new SockJS('http://localhost:8080/websocket-endpoint');
+    console.log("esmial: socked added")
+    this.stompClient = Stomp.over(socket);
+    this.stompClient.connect({}, () => {
+      this.stompClient.subscribe('/topic/updates', (update) => {
+        console.log("here in update ")
+        console.log(JSON.parse(update.body))
+        
+      });
+    });
   },
   mounted() {
     this.stage = new Konva.Stage({
@@ -191,6 +206,25 @@ export default {
           return;
         }
       }
+      for (let arrow of this.arrows) {
+        let tmp=arrow[0] + '>' + arrow[1];
+        this.arrowsGroup.push(tmp);
+
+      }
+      console.log("arrows group")
+       console.log(this.arrowsGroup)
+      fetch(`http://localhost:8080/start?machines=${this.machines.length}&queues=${this.queues.length}
+             &arrows=${this.arrowsGroup}&numberofProducts=${this.NumberofProducts}`, {
+        method: 'POST',
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          console.log("Machines and Queues and arrows send successfully")
+        })
+        .catch(error => {
+          console.error('Error:', error)
+        });
       alert("Playing...");
     },
     replay() {

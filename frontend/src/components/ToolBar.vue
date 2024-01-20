72f7007c3fbce36ef31f94d4b9eba11a243e58bf
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2024-01-20 16:00:29 +0200

    update arrows

--- frontend/src/components/ToolBar.vue
+++ frontend/src/components/ToolBar.vue
@@ -9,31 +9,51 @@
           <label>Queue</label>
         </div>
         <div class="btn">
-          <button class="pi pi-arrow-up-right" @click="this.$emit('addArrow')"></button>
+          <button class="pi pi-arrow-up-right" :class="{selected: isAddingArrow}" @click="this.$emit('addArrow')"></button>
           <label>Thread</label>
         </div>
         <div class="btn">
-          <button class="pi pi-play"></button>
+          <button class="pi pi-play" @click="this.$emit('Play')"></button>
           <label>Play</label>
         </div>
         <div class="btn">
-          <button class="pi pi-replay"></button>
+          <button class="pi pi-replay" @click="this.$emit('Replay')"></button>
           <label>Replay</label>
         </div>
         <div class="btn">
-          <button class="pi pi-plus"></button>
+          <button class="pi pi-plus" @click="this.$emit('addProduct')"></button>
           <label>Product</label>
         </div>
-    </div>
+      </div>
+      <div v-if="NumberofProducts>0" class="minbtn">
+        <button class="pi pi-minus" @click="this.$emit('remProduct')"></button>
+      </div>
+      <div class="prods">
+          Number of products: {{ NumberofProducts  }}
+      </div>
 </template>
 <script>
 import 'primeicons/primeicons.css';
 
 export default {
   name: 'ToolBar',
+  props: {
+    isAddingArrow: Boolean,
+    NumberofProducts: Number,
+  },
+  data() {
+    return {
+      addingArrow: false,
+    }
+  }
 }
 </script>
 <style scoped>
+.selected{
+  border: 4px solid rgb(9, 218, 9) !important;
+  border-radius: 10px !important;
+  background-color: aquamarine !important;
+}
 .ToolBar {
   display: flex;
   flex-direction: row;
@@ -47,6 +67,17 @@ export default {
   width: 50%;
   height: 8%;
 }
+.prods{
+  position: absolute;
+  bottom: 2.4%;
+  right: 26.8%;
+  font-size: 2vh;
+  background-color: rgba(46, 191, 239, 0.68);
+  border-bottom-left-radius: 1vh;
+  border-bottom-right-radius: 1vh;
+  border-top: 0.3vh solid rgb(48, 157, 234);
+  padding: 1vh;
+}
 .btn{
   display: flex;
   flex-direction: column;
@@ -55,6 +86,12 @@ export default {
   width: 7%;
   height: 100%;
   padding-top: 0.5%;
+}
+.minbtn{
+  position: absolute;
+  bottom: 6%;
+  right: 27%;
+  height: 5%;
 }
 label{
   font-size: 1.5vh;
